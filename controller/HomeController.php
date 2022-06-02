3b885f02efc9ff49f2ec2d3f04260687ad54978d
Gaston Santos <gastons525@gmail.com>
2022-06-02 15:10:22 -0300

    as

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -13,31 +13,13 @@ class HomeController{
 
     function show(){
 
-
-        if(!isset($_SESSION["logueado"]) || (isset($_SESSION["logueado"]) && !$_SESSION["logueado"] )){
         
-            $data["viajes"] = $this->vuelosModel->getVuelos();
-            
-            echo $this->printer->render("homeView.html", $data);
-            exit();
-           } 
-             else if (isset($_SESSION["esAdmin"])) {
-    
-                $data["esAdmin"] = $_SESSION["esAdmin"];
-                $data["viajes"] = $this->vuelosModel->getVuelos();
-                echo $this->printer->render("homeView.html", $data);
-                exit();
-            }else if(isset($_SESSION["esClient"])){
-            
-                $data["esClient"] = $_SESSION["esClient"];
-                $data["viajes"] = $this->vuelosModel->getVuelos();
-                echo $this->printer->render("homeView.html", $data);
-                exit();
-            } 
-        }
+        $data["viajes"] = $this->vuelosModel->getVuelos();
         
-        
+        echo $this->printer->render("homeView.html", $data);
+      
       
     }
+}
 
 
