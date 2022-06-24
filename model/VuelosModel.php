bc8d97850e27b86f01a2f5b09fddb887b9428484
Eri-random <brenherrera@alumno.unlam.edu.ar>
2022-06-23 22:22:54 -0300

    agregando duracion de vuelos

--- model/VuelosModel.php
+++ model/VuelosModel.php
@@ -23,7 +23,7 @@ class VuelosModel {
     }
 
     public function getVuelosTest() {
-        $results = $this->database->query("SELECT v.id, v.lugar_partida,v.destino,v.precio,tipo_viaje.nombre,recorrido.parada, DATE_FORMAT(fecha_partida, '%d-%m-%Y') fecha_partida,DATE_FORMAT(hora, '%H:%i') hora FROM vuelo as v JOIN tipo_viaje ON v.id_tipo_viaje = tipo_viaje.id LEFT JOIN recorrido ON v.id_tipo_equipo = recorrido.id_tipo_equipo AND v.id_tipo_viaje = recorrido.id_tipo_viaje AND v.activo = true");
+        $results = $this->database->query("SELECT v.id, v.lugar_partida,v.destino,v.precio,v.id_tipo_equipo,v.id_tipo_viaje,tipo_equipo.nombre,recorrido.parada, DATE_FORMAT(fecha_partida, '%a. %d %M. %Y') fecha_partida,DATE_FORMAT(hora, '%H:%i') hora FROM vuelo as v JOIN tipo_equipo ON v.id_tipo_equipo = tipo_equipo.id LEFT JOIN recorrido ON v.id_tipo_equipo = recorrido.id_tipo_equipo AND v.id_tipo_viaje = recorrido.id_tipo_viaje AND v.activo = true");
 
         return $results;
     }
